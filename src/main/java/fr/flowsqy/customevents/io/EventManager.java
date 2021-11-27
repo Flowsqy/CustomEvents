5f0c89e504283935b61e697b42cb227cfb977b7e
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2021-11-27 20:22:11 +0100

    Split #getMillisBefore for future

--- src/main/java/fr/flowsqy/customevents/io/EventManager.java
+++ src/main/java/fr/flowsqy/customevents/io/EventManager.java
@@ -4,13 +4,11 @@ import java.util.Calendar;
 
 public class EventManager {
 
-    private long getMillisBefore(final Calendar now, int dayOfWeek, int hours, int minutes) {
+
+    private Calendar getNextDay(final Calendar now, int dayOfWeek){
         final Calendar nextDay = (Calendar) now.clone();
         nextDay.add(Calendar.DAY_OF_WEEK, dayOfWeek - now.get(Calendar.DAY_OF_WEEK) % 7);
-        nextDay.set(Calendar.HOUR_OF_DAY, hours);
-        nextDay.set(Calendar.MINUTE, minutes);
-
-        return nextDay.getTimeInMillis() - now.getTimeInMillis();
+        return nextDay;
     }
 
 }
