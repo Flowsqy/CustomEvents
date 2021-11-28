8916845dd9ce0523baf61e7542ad4ccca0bc0609
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2021-11-28 19:25:12 +0100

    Deserialize an event file

--- src/main/java/fr/flowsqy/customevents/io/EventManager.java
+++ src/main/java/fr/flowsqy/customevents/io/EventManager.java
@@ -1,36 +1,40 @@
 package fr.flowsqy.customevents.io;
 
+import fr.flowsqy.customevents.api.Event;
 import fr.flowsqy.customevents.api.EventDeserializer;
+import fr.flowsqy.customevents.event.EventChain;
+import fr.flowsqy.customevents.event.EventDater;
 import fr.flowsqy.customevents.event.EventQueue;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class EventManager {
 
-    private Map<String, EventDeserializer> events;
+    private Map<String, EventDeserializer> deserializers;
     private boolean register;
-    private final EventQueue eventQueue;
+    private EventQueue eventQueue;
 
     public EventManager() {
-        this.events = new HashMap<>();
+        this.deserializers = new HashMap<>();
         this.register = true;
         this.eventQueue = null;
     }
 
-    public EventDeserializer register(String event, EventDeserializer eventDeserializer, boolean force) {
+    public EventDeserializer register(String type, EventDeserializer deserializer, boolean force) {
         if (!register) {
             throw new IllegalStateException("Can not register a deserializer after plugin loading");
         }
         if (force) {
-            return events.put(event, eventDeserializer);
+            return deserializers.put(type, deserializer);
         }
-        return events.putIfAbsent(event, eventDeserializer);
+        return deserializers.putIfAbsent(type, deserializer);
     }
 
-    public void init(Logger logger, File eventFolder) {
+    public void init(Logger logger, File eventFolder, Calendar now) {
         register = false;
         if (!eventFolder.canRead() || !eventFolder.isDirectory()) {
             logger.warning(eventFolder.getAbsolutePath() + " is not a readable directory");
@@ -39,14 +43,55 @@ public class EventManager {
         if (files == null || files.length == 0) {
             return;
         }
+        final List<EventChain> chains = new ArrayList<>();
         for (File file : files) {
-            initEvent(file, logger);
+            final EventChain chain = initEvent(file, logger, now);
+            if (chain != null) {
+                chains.add(chain);
+            }
         }
-        events = null;
+        if (!chains.isEmpty()) {
+            eventQueue = new EventQueue(chains, now.getTimeInMillis());
+        }
+        deserializers = null;
     }
 
-    private void initEvent(File file, Logger logger) {
+    private EventChain initEvent(File file, Logger logger, Calendar now) {
+        final YamlConfiguration configuration = YamlConfiguration.loadConfiguration(file);
+        final String eventType = configuration.getString("type");
+        final String fileName = file.getName();
+        if (eventType == null) {
+            logger.warning(fileName + " try to load an event without type");
+            return null;
+        }
+        final EventDeserializer deserializer = deserializers.get(eventType);
+        if (deserializer == null) {
+            logger.warning(fileName + " try to register an event that can not be deserialized (" + eventType + ")");
+            return null;
+        }
+        final ConfigurationSection eventSection = configuration.getConfigurationSection("event");
+        if (eventSection == null) {
+            logger.warning(fileName + " does not comport 'event' section");
+            return null;
+        }
+        final Event event = deserializer.deserialize(eventSection, logger);
+        if (event == null) {
+            return null;
+        }
+        final ConfigurationSection dateSection = configuration.getConfigurationSection("date");
+        if (dateSection == null) {
+            logger.warning(fileName + " doest not comport 'date' section");
+            return null;
+        }
+        final EventDater dater = initDate(dateSection, logger, now);
+        if (dater == null) {
+            return null;
+        }
+        return new EventChain(event, dater);
+    }
 
+    private EventDater initDate(ConfigurationSection dateSection, Logger logger, Calendar now) {
+        return null;
     }
 
 }
