1adf8f4cf49cc631caa6ec4d1d3debf19cb5b866
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2022-02-20 13:50:53 +0100

    Implement start subcommand

--- src/main/java/fr/flowsqy/customevents/command/subcommand/StartSubCommand.java
+++ src/main/java/fr/flowsqy/customevents/command/subcommand/StartSubCommand.java
@@ -1,6 +1,7 @@
 package fr.flowsqy.customevents.command.subcommand;
 
 import fr.flowsqy.customevents.CustomEventsPlugin;
+import fr.flowsqy.customevents.api.Event;
 import fr.flowsqy.customevents.command.SubCommand;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -9,13 +10,28 @@ import java.util.List;
 
 public class StartSubCommand extends SubCommand {
 
+    private final CustomEventsPlugin plugin;
+
     public StartSubCommand(CustomEventsPlugin plugin, String name, String permission, String... aliases) {
         super(plugin, name, permission, aliases);
+        this.plugin = plugin;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+        if (args.length == 2) {
+            final String arg = args[1];
+            for (Event event : plugin.getCycleManager().getEvents()) {
+                if (event.getData().getCommandId().equals(arg)) {
+                    plugin.getMessages().sendMessage(sender, "command.start.success", "%event%", arg);
+                    event.perform();
+                    return true;
+                }
+            }
+            return plugin.getMessages().sendMessage(sender, "command.start.fail", "%event%", arg);
+        }
+        sender.sendMessage(helpMessage);
+        return true;
     }
 
     @Override
--- src/main/java/fr/flowsqy/customevents/event/manager/CycleManager.java
+++ src/main/java/fr/flowsqy/customevents/event/manager/CycleManager.java
@@ -1,23 +1,24 @@
 package fr.flowsqy.customevents.event.manager;
 
 import fr.flowsqy.customevents.CustomEventsPlugin;
+import fr.flowsqy.customevents.api.Event;
 import fr.flowsqy.customevents.event.queue.EventQueue;
 
 import java.io.File;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Locale;
+import java.util.*;
 
 public class CycleManager {
 
     private final CustomEventsPlugin plugin;
     private final Locale locale;
     private final TaskManager taskManager;
+    private final List<Event> events;
 
     public CycleManager(CustomEventsPlugin plugin, Locale locale) {
         this.plugin = plugin;
         this.locale = locale;
         taskManager = new TaskManager();
+        events = new LinkedList<>();
     }
 
     public void load() {
@@ -27,11 +28,15 @@ public class CycleManager {
                 new File(plugin.getDataFolder(), "events"),
                 now
         );
+        for (Event event : eventQueue) {
+            events.add(event);
+        }
         taskManager.initialize(plugin, eventQueue, now);
     }
 
     public void unload() {
         taskManager.cancel();
+        events.clear();
     }
 
     public void reload() {
@@ -39,4 +44,7 @@ public class CycleManager {
         load();
     }
 
+    public List<Event> getEvents() {
+        return events;
+    }
 }
